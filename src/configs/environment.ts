a5c4a5b545abe4951bb501416217659d2abfa44e
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-07-16 11:43:33 +0200

    Setting project architecture and firebase connection

---
+++ src/configs/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBkzu3UGHZXNWWkjifqXt7lR7GaFa63qJI",
+  authDomain: "otaku-app-1fadb.firebaseapp.com",
+  projectId: "otaku-app-1fadb",
+  storageBucket: "otaku-app-1fadb.appspot.com",
+  messagingSenderId: "279211548640",
+  appId: "1:279211548640:web:db266f6efe16473a2bd585"
+};

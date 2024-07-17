772b4939f7595e35ad683d9b637c7462f499191a
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-07-17 17:46:18 +0200

    Setting up footer component UI

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { LanguageSwitcherComponent } from "../language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [LanguageSwitcherComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })

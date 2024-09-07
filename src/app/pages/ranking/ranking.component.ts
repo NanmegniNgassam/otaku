b940e02cc8cc7e16b3a48989cf9d72713be59388
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-09-07 12:05:35 +0200

    Setting up responsivity and translations

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, TranslateModule],
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]

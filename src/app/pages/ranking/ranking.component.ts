a662e47e43bca7edb96b81ee956bc848caeb0e53
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-09-07 07:53:27 +0200

    Setting ranking UI

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './ranking.component.html',
-  styleUrl: './ranking.component.scss'
+  styleUrl: './ranking.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RankingComponent {
+  _isSearching = false;
+
+
+  onSearch(searchValue: string) {
+    if(!searchValue)
+      return;
+
+    this._isSearching = true;
+  }
 
 }

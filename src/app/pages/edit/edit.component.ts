f7b42c90703f3e138a5b1e4c7d5bc34044aa32ac
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-10-11 04:31:09 +0200

    Add a return UI component in account page

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -2,11 +2,12 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AvatarEditComponent } from '../../components/avatar-edit/avatar-edit.component';
 import { UserFormComponent } from '../../components/user-form/user-form.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [ TranslateModule, UserFormComponent, AvatarEditComponent],
+  imports: [ TranslateModule, UserFormComponent, AvatarEditComponent, RouterModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]

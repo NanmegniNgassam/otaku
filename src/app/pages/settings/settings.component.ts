0eb08cfc608ea76fc99b00b6fac45ea2d0657229
Gilles NGASSAM <nanmegningassam@gmail.com>
2024-11-27 06:04:49 +0100

    feature: forbidden genres management

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -4,6 +4,9 @@ import { TranslateModule } from '@ngx-translate/core';
 import { UtilsService } from '../../services/utils.service';
 import AuthService from '../../services/auth.service';
 import { AsyncPipe } from '@angular/common';
+import { EXPLICIT_CONTENT_GENRES } from '../../services/anime.service';
+import { UserService } from '../../services/user.service';
+import { UserData } from '../../models/user';
 
 @Component({
   selector: 'app-settings',
@@ -15,12 +18,75 @@ import { AsyncPipe } from '@angular/common';
 })
 export class SettingsComponent {
   _user$ = this.auth.user$;
+  _forbiddenGenres: string[] = [];
+  _safeGenres: string[] = [];
+  _userData!: UserData;
+  _isWaitingSave: boolean = false;
+
+  /**
+   * 
+   * @param auth auth service
+   * @param translate translate service
+   * @param user user/db service
+   */
   constructor(
     protected util:UtilsService,
-    private auth:AuthService
+    private auth:AuthService,
+    private user:UserService,
   ) {
   }
+
+  /**
+   * Performs some general tasks right after construction
+   */
+  async ngOnInit() {
+    this._userData = await this.user.fetchUserData();
+    
+    this._forbiddenGenres = [...this._userData.params.forbiddenGenres.sort()];
+    this._safeGenres = EXPLICIT_CONTENT_GENRES.filter(genre => !this._forbiddenGenres.includes(genre)).sort();
+  }
+
+  /**
+   * Updates the genres list (forbidden or safe)
+   */
+  updateGenres = (genre: string, actionType: 'authorize' | 'forbid'): void => {
+    if (actionType === 'authorize') {
+      this._forbiddenGenres = this._forbiddenGenres.filter(g => g !== genre);
+      this._safeGenres.push(genre);
+      this._safeGenres.sort();
+    } else {
+      this._safeGenres = this._safeGenres.filter(g => g !== genre);
+      this._forbiddenGenres.push(genre);
+      this._forbiddenGenres.sort();
+    }
+    this._isWaitingSave = this.isSaveble();
+  }
+
+  /**
+   * Check if it's any changes in the forbidden genres list
+   */
+  isSaveble = (): boolean => {
+    if(this._userData) {
+      return !this.util.isArraysIdentical<string>(this._userData.params.forbiddenGenres, this._forbiddenGenres)
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Saves the changes made in the forbidden genres list
+   */
+  saveChanges = async () => {
+    if(this.isSaveble()) {
+      try {
+        this._userData.params.forbiddenGenres = [...this._forbiddenGenres];
+        await this.user.updateUserDoc({ params: this._userData.params});
+        this._isWaitingSave = false;
+      } catch (error) {
+        console.error("Error while saving the forbidden list : ", error);
+      }
+    }
+  }
 }
-// TODO: Proposer à l'utilisateur de spécifier les genres qu'il n'aimerait pas voir lors de sa navigation (option recherche)
 // TODO: Mettre la fonctionnalité de photo de profil dans un composant à part.
 // TODO: Créer une modale pour la validation de la suppression de compte

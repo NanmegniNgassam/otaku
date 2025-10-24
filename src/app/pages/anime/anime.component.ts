f0365acbb5fc805dc5f8d4a17f40ef6b86d19711
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-10-24 23:30:56 +0200

    Dev: Fetching single anime

--- src/app/pages/anime/anime.component.ts
+++ src/app/pages/anime/anime.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AnimeService } from '../../services/anime.service';
+import { Anime } from '../../models/anime';
 
 @Component({
   selector: 'app-anime',
@@ -10,11 +12,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AnimeComponent implements OnInit {
   protected animeId!: number;
+  protected anime!: Anime;
+
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private animeService: AnimeService
   ) { }
 
   async ngOnInit() {
     this.animeId = this.route.snapshot.params['id'];
+    this.anime = await this.animeService.getAnimeById(this.animeId);
+
+    console.log("Anime fetched : ", this.anime);
   }
 }

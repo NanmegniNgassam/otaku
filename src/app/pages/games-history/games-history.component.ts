9c3a822acf1c834f2717cefbcb9e5d1b6be91fbb
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-11-12 13:08:16 +0100

    Dev: Preparing game history layout

--- src/app/pages/games-history/games-history.component.ts
+++ src/app/pages/games-history/games-history.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { Notification } from '../../models/user';
 
 @Component({
   selector: 'app-games-history',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './games-history.component.html',
-  styleUrl: './games-history.component.scss'
+  styleUrl: './games-history.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GamesHistoryComponent {
-
+  notificationsGroups!: Notification[][]
 }

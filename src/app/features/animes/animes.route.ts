0e5b5134de704f3a6a2591fcbc811efda86f5403
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-12-27 10:02:06 +0100

    Refacto: Cleaning services from component

--- src/app/features/animes/animes.route.ts
+++ src/app/features/animes/animes.route.ts
@@ -1,8 +1,8 @@
+import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, Routes } from "@angular/router";
 import { AuthGuard } from "../../core/guards/auth.guard";
-import { inject } from "@angular/core";
-import { AnimeService } from "./services/anime.service";
 import { UserService } from "../../services/user.service";
+import { AnimeService } from "./services/anime.service";
 
 export default [
   {
@@ -15,7 +15,16 @@ export default [
     canActivate: [AuthGuard],
     loadComponent: () => import("./pages/my-list/my-list.component")
       .then(m => m.MyListComponent),
-    title: "Otaku | My animes List"
+    title: "Otaku | My animes List",
+    resolve: {
+      animes: async () => {
+        const animeService = inject(AnimeService);
+        const userData = await inject(UserService).fetchUserData();
+        const animes = await animeService.getAnimeByIds(userData.animeListIds);
+
+        return animes;
+      } 
+    }
   },
   {
     path: ':id',
--- src/app/features/animes/pages/my-list/my-list.component.ts
+++ src/app/features/animes/pages/my-list/my-list.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../../../services/user.service';
-import { Anime } from '../../models/anime';
-import { UserData } from '../../../../models/user';
+import { Component, input } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AnimesListComponent } from '../../components/animes-list/animes-list.component';
-import { AnimeService } from '../../services/anime.service';
+import { Anime } from '../../models/anime';
 
 @Component({
     selector: 'app-my-list',
@@ -12,20 +9,9 @@ import { AnimeService } from '../../services/anime.service';
     templateUrl: './my-list.component.html',
     styleUrl: './my-list.component.scss'
 })
-export class MyListComponent implements OnInit {
-  animes: Anime[] | null = null;
-  currentUser!:UserData;
+export class MyListComponent {
+  animes = input.required<Anime[]>();
   protected skeletons: undefined[] = Array(10).fill(undefined);
 
-  constructor(
-    private user: UserService,
-    private anime: AnimeService
-  ) {}
-
-  async ngOnInit() {
-    this.currentUser = await this.user.fetchUserData();
-
-    const animeIds = [...this.currentUser.animeListIds];
-    this.animes = await this.anime.getAnimeByIds(animeIds);
-  }
+  constructor() {}
 }

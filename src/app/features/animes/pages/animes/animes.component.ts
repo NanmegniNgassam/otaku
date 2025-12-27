eb03c39887ddfd456e91918c1563eaa37ad4a9cb
Gilles NGASSAM <nanmegningassam@gmail.com>
2025-12-27 10:24:41 +0100

    Refacto: Cleaning animes page component

--- src/app/features/animes/pages/animes/animes.component.ts
+++ src/app/features/animes/pages/animes/animes.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Anime } from '../../models/anime';
-import AuthService from '../../../../core/services/auth.service';
+import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { AnimesListComponent } from '../../components/animes-list/animes-list.component';
+import { Anime } from '../../models/anime';
 import { AnimeService } from '../../services/anime.service';
 
 @Component({
     selector: 'app-landing',
     imports: [AnimesListComponent, TranslateModule],
     templateUrl: './animes.component.html',
-    styleUrl: './animes.component.scss'
 })
 export class AnimesComponent {
   protected animes:Anime[] | null = null;
   protected skeletons: undefined[] = Array(20).fill(undefined);
 
   constructor(
-    protected auth: AuthService,
     protected anime: AnimeService
   ) {
   }
@@ -24,7 +21,5 @@ export class AnimesComponent {
   async ngOnInit(): Promise<void> {
     const animes = await this.anime.getTopAnimes();
     this.animes = animes;
-
-    console.log("Randomly fetched animes : ", animes);
   }
 }

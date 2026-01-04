78a9fb20304304d98b9537df2c0df2db49c58bb8
Gilles NGASSAM <nanmegningassam@gmail.com>
2026-01-04 16:10:38 +0100

    upgrade: Installing vitest as test runner

---
+++ src/test-setup.ts
@@ -0,0 +1,10 @@
+import { getTestBed } from '@angular/core/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import 'zone.js';
+import 'zone.js/testing';
+
+// Initialiser l'environnement de test Angular
+getTestBed().initTestEnvironment(
+  BrowserTestingModule,
+  platformBrowserTesting(),
+);

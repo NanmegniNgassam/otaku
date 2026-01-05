15559152445e916d62211b071e301cb62fd3fc69
Gilles NGASSAM <nanmegningassam@gmail.com>
2026-01-05 21:54:12 +0100

    Model: Add character model for anime

---
+++ src/app/features/animes/models/character.ts
@@ -0,0 +1,15 @@
+export interface Character {
+  mal_id: number;
+  url: string;
+  images: {
+    jpg : {
+      image_url: string;
+      small_image_url: string;
+    }
+    webp: {
+      image_url: string;
+      small_image_url: string;
+    }
+  };
+  name: string;
+}
